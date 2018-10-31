c1c81d35df70df6c07eed2412f4850f13fec7179
Jeremiah <jeremiah3643@gmail.com>
2018-10-31 14:05:40 -0500

    model done

---
+++ BangazonAPI/Models/TrainingProgram.cs
@@ -0,0 +1,24 @@
+﻿/*
+    Author: Jeremiah Pritchard
+    Purpose: Model for TrainingProgram class to represent training programs from the DB.
+    
+*/
+
+using BangazonAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+
+    public class TrainingProgram
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MaxAttendees { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
